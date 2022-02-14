be26e335362418f8c7b0c26b8d42db4fe4d3c66f
xaviac <jacevedomx@gmail.com>
2022-02-14 14:50:04 -0600

    Kata módulo 10

---
+++ Módulo_10_Manejo_de_errores/open.py
@@ -0,0 +1,30 @@
+def main():
+    try:
+        configuration = open('config.py')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+
+
+if __name__ == '__main__':
+    main()
+
+""" def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+
+
+if __name__ == '__main__':
+    main() """
+
+""" try:
+    open('config.txt')
+except FileNotFoundError:
+    print("Couldn't find the config.txt file!") """
+
+""" def main():
+    open("/path/to/mars.jpg")
+
+if __name__ == '__main__':
+    main() """
